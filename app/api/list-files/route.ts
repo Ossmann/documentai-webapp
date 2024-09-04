3525c47878bfa75f8e11e03a0512e170a59c14a0
Jakob Ossmann <Jakobossmann@gmail.com>
2024-09-04 10:52:53 +1000

    signed urls for file download

--- app/api/list-files/route.ts
+++ app/api/list-files/route.ts
@@ -8,10 +8,8 @@ export async function GET() {
     };
 
     try {
-        // Fetch the list of objects from the S3 bucket
         const data = await s3.listObjectsV2(params).promise();
 
-        // Check if no files are found and return an empty array with cache-control headers
         if (!data.Contents || data.Contents.length === 0) {
             console.log('No files found in the specified folder.');
             return NextResponse.json({ files: [] }, {
@@ -23,13 +21,19 @@ export async function GET() {
             });
         }
 
-        // Map the list of files from the S3 response
-        const files = data.Contents.map(item => ({
-            key: item.Key,
-            url: `https://${params.Bucket}.s3.${process.env.AWS_DEFAULT_REGION}.amazonaws.com/${item.Key}`
+        const files = await Promise.all(data.Contents.map(async (item) => {
+            const signedUrl = await s3.getSignedUrlPromise('getObject', {
+                Bucket: params.Bucket,
+                Key: item.Key,
+                Expires: 60 * 5 // URL valid for 5 minutes
+            });
+
+            return {
+                key: item.Key,
+                url: signedUrl // Use the signed URL instead of the direct URL
+            };
         }));
 
-        // Return the files with appropriate cache-control headers
         return NextResponse.json({ files }, {
             headers: {
                 'Cache-Control': 'no-store, no-cache, must-revalidate, proxy-revalidate, max-age=0, s-maxage=0, stale-while-revalidate=0',
@@ -37,13 +41,8 @@ export async function GET() {
                 'Expires': '0'
             }
         });
-
     } catch (error) {
-        console.error('Error listing files:', error); // Log the error
-        if (error instanceof Error) {
-            return NextResponse.json({ error: error.message }, { status: 500 });
-        } else {
-            return NextResponse.json({ error: 'An unknown error occurred' }, { status: 500 });
-        }
+        console.error('Error listing files:', error);
+        return NextResponse.json({ error: 'Error listing files' }, { status: 500 });
     }
 }
